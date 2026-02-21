62f4037ff93f506c4023792412867d96a70f1df8
greatmanfred <greatmanfred2011@gmail.com>
2026-02-21 08:58:20 +0000

    Update TradingBot.py

--- TradingBot.py
+++ TradingBot.py
@@ -126,8 +126,8 @@ def fetch_5m_data(symbol, weeks_required=2):
         print(f"[FETCH DATA KEYS] {symbol}: {list(data.keys())}")
 
     if "values" not in data:
-        print(f"[ERROR] No values returned for {symbol}")
-        return None
+    print(f"[FULL API ERROR] {symbol} -> {data}")
+    return None
 
     df = pd.DataFrame(data["values"])
     df = df.astype(float)
